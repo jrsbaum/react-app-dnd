9b687327db3580bdfeed857a1516be17477acb91
Júnior Baum <juniorsbaum@hotmail.com>
2022-11-03 23:23:01 -0300

    feat: add react beautiful dnd, bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const data = [
+  {
+    id: "item-1",
+    content: "Item-1",
+  },
+  {
+    id: "item-2",
+    content: "Item-2",
+  },
+  {
+    id: "item-3",
+    content: "Item-3",
+  },
+  {
+    id: "item-4",
+    content: "Item-4",
+  },
+];
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+const getListStyle = (isDragginOver) => ({
+  background: isDragginOver ? "lightblue" : "lightgrey",
+  padding: 8,
+  height: 500,
+  width: 250,
+});
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(data);
+  }, []);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const reorderedItems = reorder(
+      items,
+      result.source.index,
+      result.destination
+    );
+
+    console.log(reorder);
+    setItems(reorderedItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppabledId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDragginOver)}
+            ></div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
