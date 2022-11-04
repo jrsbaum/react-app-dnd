d180e6c00a045188944020f53abca67371a42344
Júnior Baum <juniorsbaum@hotmail.com>
2022-11-04 00:35:12 -0300

    feat: list dnd w/ animals

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
+import React, { useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
-import { useEffect, useState } from "react";
 
-const data = [
+const animals = [
   {
     id: "item-1",
-    content: "Item-1",
+    content: "Cachorro",
   },
   {
     id: "item-2",
-    content: "Item-2",
+    content: "Gato",
   },
   {
     id: "item-3",
-    content: "Item-3",
+    content: "Onça",
   },
   {
     id: "item-4",
-    content: "Item-4",
+    content: "Leão",
+  },
+  {
+    id: "item-5",
+    content: "Capivara",
+  },
+  {
+    id: "item-6",
+    content: "Onça",
+  },
+  {
+    id: "item-7",
+    content: "Cabra",
+  },
+  {
+    id: "item-8",
+    content: "Abutre",
+  },
+  {
+    id: "item-9",
+    content: "Jabuti",
   },
 ];
-
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
+
   return result;
 };
 
-const getListStyle = (isDragginOver) => ({
-  background: isDragginOver ? "lightblue" : "lightgrey",
-  padding: 8,
-  height: 500,
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging ? "lightgreen" : "grey",
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
   width: 250,
 });
 
@@ -39,7 +68,7 @@ const App = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    setItems(data);
+    setItems(animals);
   }, []);
 
   const onDragEnd = (result) => {
@@ -50,23 +79,43 @@ const App = () => {
     const reorderedItems = reorder(
       items,
       result.source.index,
-      result.destination
+      result.destination.index
     );
 
-    console.log(reorder);
+    console.log({ reorderedItems });
     setItems(reorderedItems);
   };
 
   return (
-    <div>
+    <div className="main_content">
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppabledId="droppable">
+        <Droppable droppableId="droppable">
           {(provided, snapshot) => (
             <div
               {...provided.droppableProps}
               ref={provided.innerRef}
-              style={getListStyle(snapshot.isDragginOver)}
-            ></div>
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      className="card"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
           )}
         </Droppable>
       </DragDropContext>
